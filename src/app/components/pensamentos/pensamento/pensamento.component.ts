32520ca50f8a0ae0b0d8e2fbe8e9ed61b77ac551
Lucas Waldes <lwcarvalho08@gmail.com>
2023-12-16 11:21:20 -0300

    feat: funcionalidade de favoritar

--- src/app/components/pensamentos/pensamento/pensamento.component.ts
+++ src/app/components/pensamentos/pensamento/pensamento.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-pensamento',
@@ -16,7 +17,9 @@ export class PensamentoComponent implements OnInit {
     favorito: false
   }
 
-  constructor() { }
+  constructor(
+    private service: PensamentoService
+  ) { }
 
   ngOnInit(): void {
   }
@@ -33,6 +36,11 @@ export class PensamentoComponent implements OnInit {
       return 'inativo'
     }
     return 'ativo'
+  }
+
+  atualizarFavoritos() {
+    this.service.mudarFavorito(this.pensamento).subscribe()
+    console.log(this.pensamento.favorito)
   }
 
 }
